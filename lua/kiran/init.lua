1fc186cb30432058d954a74a9f3eb1d97a2ac176
bitesized <kiran.sangha@outlook.com>
2024-09-04 13:21:52 +1200

    change order

--- lua/kiran/init.lua
+++ lua/kiran/init.lua
@@ -1,6 +1,6 @@
+require("kiran.packer")
 require("kiran.remap")
 require("kiran.set")
-require("kiran.packer")
 -- Theme
 vim.o.background = "dark" -- or "light" for light mode
 vim.cmd([[colorscheme gruvbox]])
